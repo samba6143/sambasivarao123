ee648b3142b180683a3d31d5008b6c7da8c711f0
samba6143 <151744013+samba6143@users.noreply.github.com>
2023-11-25 16:50:06 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Demo {
+	
+	static int a=50;
+	static int b=10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Sum=a-b;
+	      System.out.println(Sum);
+
+	}
+
+}
---
+++ Demo3.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//increment
+		//Per increment++i;
+		//post increment i++;
+		int a=25;
+		int b=30;
+		int c =--a;
+		System.out.println(c);
+		int d =++b;
+		System.out.println(d);
+		
+		
+	b++;
+	System.out.println(b);
+	
+		
+		
+		
+		
+	}
+
+}
